1b71dc5af3f064c87cb739c644bb4a94af70c7b2
Patrick Adutwum <adutwum.pa@gmail.com>
2021-12-29 12:35:55 +0000

    implement settings nav

--- app/src/main/java/com/example/e_commence/fragment/Settings.kt
+++ app/src/main/java/com/example/e_commence/fragment/Settings.kt
@@ -1,17 +1,36 @@
 package com.example.e_commence.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.e_commence.R
+import androidx.fragment.app.Fragment
+import com.example.e_commence.Login
+import com.example.e_commence.MainActivity
+import com.example.e_commence.api.Network
+import com.example.e_commence.databinding.SettingsBinding
+import com.example.e_commence.showToast
+import com.example.e_commence.utils.Constants
+import com.example.e_commence.utils.Services
+import com.example.e_commence.utils.SessionManager
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.toRequestBody
 
 
 class Settings : Fragment() {
+    private var _binding: SettingsBinding? = null
+    private val binding get() = _binding!!
+
+    lateinit var sessionManager: SessionManager
+    private val client = OkHttpClient()
+    var services: Services? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sessionManager = SessionManager(requireActivity())
+        services = MainActivity.services
     }
 
     override fun onCreateView(
@@ -19,10 +38,47 @@ class Settings : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings, container, false)
+        _binding = SettingsBinding.inflate(inflater, container, false)
+
+        init()
+
+        return binding.root
     }
 
-    companion object {
+    private fun init(){
+        with(binding){
+            name.text = if (sessionManager.isRem()){
+                sessionManager.userFullName()
+            }else{
+                services?.user?.name
+            }
 
+            email.text = if (sessionManager.isRem()){
+                sessionManager.userEmail()
+            }else{
+                services?.user?.email
+            }
+
+            logoutBtn.setOnClickListener {
+                progress.visibility = View.VISIBLE
+                Network.makeRequest(Constants.logout, "".toRequestBody(), client, services?.tokenCheck()!!).doOnError {
+                    it.printStackTrace()
+                }.subscribe{
+                    activity?.runOnUiThread {
+                        println("logout result: $it")
+                        if (it.has("message")){
+                            if (it.getString("message").equals("Logged Out")){
+                                sessionManager.logout()
+                                startActivity(Intent(requireActivity(), Login::class.java))
+                                activity?.finish()
+                            }else{
+                                activity?.showToast(it.getString("message"))
+                            }
+                        }
+                        progress.visibility = View.INVISIBLE
+                    }
+                }
+            }
+        }
     }
 }
